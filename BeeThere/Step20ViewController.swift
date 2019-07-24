95d8da26beec39d8ff89cae9b1a0ab16df08de39
堀川浩二 <>
2019-07-25 00:07:57 +0800

    機能画面プロトタイプ1

--- BeeThere/Step20ViewController.swift
+++ BeeThere/Step20ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class Step20ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var question = ["なぜなぜ1回目", "なぜなぜ2回目", "なぜなぜ3回目", "なぜなぜ4回目", "なぜなぜ5回目", "なぜなぜ6回目", "なぜなぜ7回目", "なぜなぜ8回目"]
+    
     var questionCounter = 1 //TableViewの「なぜ」の数を数える
     
     
@@ -17,7 +19,7 @@ class Step20ViewController: UIViewController, UITableViewDelegate, UITableViewDa
     @IBOutlet weak var tableView: UITableView!
 
     
-    var value = "まだ見つけていません"
+    var value: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,24 +30,24 @@ class Step20ViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Do any additional setup after loading the view.
         textFromStep11.text = value
         
-        var question = ["なぜなぜ1回目", "なぜなぜ2回目", "なぜなぜ3回目", "なぜなぜ4回目", "なぜなぜ5回目", "なぜなぜ6回目", "なぜなぜ7回目", "なぜなぜ8回目"]
-
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return question.count
-            
-            print(question)
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
-            cell.textLabel?.text = question[indexPath.row]
-            return cell
-        }
-        
+    
         
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return question.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        cell.textLabel?.text = question[indexPath.row]
+        return cell
+    }
+    
 
 }
+
+
 
